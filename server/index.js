73e3d20a56b3752b6bf36745a01370f5d360fcbc
Curran-C <currandoddabele@gmail.com>
2022-05-28 22:54:07 +0530

    inital commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+
+app.use(cors())
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
+
+io.on('connection', (socket) => {
+
+    console.log(`User ${socket.id} connected`);
+
+    socket.on('join_room', (data) => {
+        socket.join(data)
+        console.log(`User with ID = ${socket.id} joined room ${data}`);
+    })
+
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit('recieve_message', data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected', socket.id);
+    })
+})
+
+server.listen(3001, () => {
+    console.log('Server started on post 3001');
+})
